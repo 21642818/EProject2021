fb6b901e29e1a3157849d1f29a879e43820f3dbb
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-06 22:34:44 +0200

    New Camera example code

--- example_code/Camera/camera.py
+++ example_code/Camera/camera.py
@@ -1,15 +1,20 @@
 from picamera import PiCamera
 from time import sleep
+import numpy as np
+import cv2
 from fractions import Fraction
 from datetime import datetime
 
-camera = PiCamera()
-camera.resolution = (3280, 2464)
-camera.start_preview()
-sleep(5)
-#now = datetime.now()
-#d = now.strftime("%m%d%Y_%H%M%S")
-filename = './img/'+datetime.now().strftime("%m%d%Y-%H%M%S")+'.jpg'
-#camera.capture('./img/'+datetime.now().strftime("%m%d%Y_%H%M%S")+'.jpg')
-camera.capture(filename)
-camera.stop_preview()
+with PiCamera() as camera:
+    camera.resolution = (3280, 2464)
+    camera.start_preview()
+    sleep(2)
+    #now = datetime.now()
+    #d = now.strftime("%m%d%Y_%H%M%S")
+    filename = './img/'+datetime.now().strftime("%m%d%Y-%H%M%S")+'.png'
+    #camera.capture('./img/'+datetime.now().strftime("%m%d%Y_%H%M%S")+'.jpg')
+    image = np.empty((2464 * 3280 * 3,), dtype=np.uint8)
+    camera.capture(image, 'bgr')
+    image = image.reshape((2464, 3280, 3))
+    cv2.imwrite(filename, image)
+    camera.stop_preview()
