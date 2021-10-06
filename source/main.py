f3d86768f114b7e3ff1f813121c55ca5d7be94cf
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-06 02:26:48 +0200

    Camera test upload

--- source/main.py
+++ source/main.py
@@ -7,7 +7,7 @@ sp = SmartPlant()
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/var/local/eproject2021-555cc-firebase-adminsdk-94yit-faec66311f.json"
 firebase = firebase.FirebaseApplication('https://eproject2021-555cc-default-rtdb.europe-west1.firebasedatabase.app/', None)
 client = storage.Client()
-bucket = client.get_bucket('https://console.firebase.google.com/project/eproject2021-555cc/storage/eproject2021-555cc.appspot.com/files')
+bucket = client.get_bucket('gs://eproject2021-555cc.appspot.com/')
 
 def update_database():
     sp.measure()
@@ -16,8 +16,8 @@ def update_database():
     print(data)
     result = firebase.post(url=folder, data=data)
     print(result)
+    imageBlob = bucket.blob('/'+data['date']+'/')
     imagePath = data["img"]
-    imageBlob = data['date']+'/'
     imageBlob.upload_from_filename(imagePath)
 
 
