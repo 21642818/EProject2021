9e482d72e9af8228c086f482114cab85832f78d6
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-07 10:24:00 +0200

    New Camera example

--- example_code/Camera/camera.py
+++ example_code/Camera/camera.py
@@ -1,14 +1,16 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
 from picamera import PiCamera
-from time import sleep
-import numpy as np
+import time
 import cv2
-from fractions import Fraction
-from datetime import datetime
-
-with PiCamera() as camera:
-    camera = PiCamera()
-    camera.resolution = (3280, 2464)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture('foo.jpg')
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+# allow the camera to warmup
+time.sleep(0.1)
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+# display the image on screen and wait for a keypress
+cv2.imsave("image.jpg", image)
+cv2.waitKey(0)
