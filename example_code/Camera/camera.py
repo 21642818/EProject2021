8337065b7cdd1594b31088a9fef3c96334aa35e6
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-07 10:30:12 +0200

    New camera example tweak

--- example_code/Camera/camera.py
+++ example_code/Camera/camera.py
@@ -4,7 +4,8 @@ from picamera import PiCamera
 import time
 import cv2
 # initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera(sensor_mode=2)
+camera = PiCamera()
+camera.resolution = (3280, 2464)
 rawCapture = PiRGBArray(camera)
 # allow the camera to warmup
 time.sleep(0.1)
