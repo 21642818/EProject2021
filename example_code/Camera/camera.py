5b2285dae58c42927c0a6f7dba04d3288d00dfd4
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-08 14:36:33 +0200

    Camera demo changes

--- example_code/Camera/camera.py
+++ example_code/Camera/camera.py
@@ -9,18 +9,20 @@ def take_photo():
     date_time = datetime.now().strftime("%m%d%Y-%H%M%S")
     filename = 'img/'+date_time+'.jpg'
     # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
-    camera.resolution = (2048, 1536)
-    rawCapture = PiRGBArray(camera)
-    # allow the camera to warmup
-    time.sleep(0.5)
-    # grab an image from the camera
-    camera.capture(rawCapture, format="bgr")
-    camera.close()
+    try:
+        camera = PiCamera()
+        camera.resolution = (2048, 1536)
+        rawCapture = PiRGBArray(camera)
+        # allow the camera to warmup
+        time.sleep(0.5)
+        # grab an image from the camera
+        camera.capture(rawCapture, format="bgr")
+    finally:
+        camera.close()
     image = rawCapture.array
     # display the image on screen and wait for a keypress
     status = cv2.imwrite(filename, image)
-    cv2.waitKey(0)
+    cv2.waitKey(0.1)
     print (status)
 
 take_photo()
