2f0435e50b63e3b32f35e00419e802282b9be0b2
GERTHARDUS MAGNUS Marais <21642818@sun.ac.za>
2021-10-18 16:36:44 +0200

    float switch test fix

--- example_code/Float/float_switch_test.py
+++ example_code/Float/float_switch_test.py
@@ -4,7 +4,7 @@ import time
 float_sw = 4
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(float_sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(float_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     state = GPIO.input(float_sw)
