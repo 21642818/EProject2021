06aa1b1c6c30bb81463f9e4a831f1125900d88e3
21642818 <21642818@sun.ac.za>
2021-10-20 23:11:56 +0200

    changes made

--- example_code/static_var.py
+++ example_code/static_var.py
@@ -1,15 +1,14 @@
-from typing import Counter
-
-
 import time
 
 def func():
-    func.counter = 0
-    if func.counter >= 8:
+    if func.counter > 8:
         func.counter = 0
     else:
         func.counter += 1
     return func.counter
 
-print(func)
-time.sleep(1)
+func.counter = 0
+
+while True:
+    print(func())
+    time.sleep(1)
